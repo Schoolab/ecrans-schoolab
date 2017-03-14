1d15c6d8ace25644d1ec87b6c0032e23f47ceab4
AlexandreJolly <accounts@alexandrejolly.com>
2017-03-14 12:20:53 +0100

    new version + gulp

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+// Include gulp @ puglins
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename');
+    pug = require('gulp-pug');
+    // plugins = require('gulp-load-plugins')();
+
+// Define base folders
+var src = 'src/',
+    dist = 'dist/';
+
+// Lint Task
+gulp.task('lint', function() {
+  return gulp.src(src + 'assets/javascripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// Compile Our Sass
+gulp.task('sass', function() {
+  return gulp.src(src + 'assets/stylesheets/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest(dist + 'css'))
+    .pipe(browserSync.stream())
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', ['copy:vendors'], function() {
+  return gulp.src([src + 'assets/javascripts/**/*.js', src + 'assets/javascripts/vendors/*.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(dist))
+    .pipe(rename('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(dist + 'js'))
+    .pipe(browserSync.stream())
+});
+
+gulp.task('pug', function() {
+  return gulp.src(src + 'views/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest(dist))
+    .pipe(browserSync.stream())
+});
+
+// Copy vendors/libs
+gulp.task('copy:vendors', function() {
+  return gulp.src(src + 'assets/javascripts/vendors/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(dist + 'js/vendors'))
+    .pipe(browserSync.stream())
+});
+
+// Copy images
+gulp.task('copy:images', function() {
+   gulp.src(src + 'assets/images/**/*.{png,jpg,jpeg,svg}')
+   .pipe(gulp.dest(dist + 'img'));
+});
+
+// Copy videos
+gulp.task('copy:videos', function() {
+   gulp.src(src + 'assets/videos/**/*.{mp4,mov}')
+   .pipe(gulp.dest(dist + 'videos'));
+});
+
+// Copy fonts
+gulp.task('copy:fonts', function() {
+   gulp.src(src + 'assets/fonts/**/*.{ttf,woff,eof,svg,otf}')
+   .pipe(gulp.dest(dist + 'fonts'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', ['browserSync', 'compile'], function() {
+  gulp.watch(src + 'assets/javascripts/**/*.js', ['lint', 'scripts']);
+  gulp.watch(src + 'assets/stylesheets/**/*.scss', ['sass']);
+  gulp.watch(src + 'assets/images/**/*.{png,jpg,jpeg,svg}', ['copy:images']);
+  gulp.watch(src + 'assets/fonts/**/*.{ttf,woff,eof,svg}', ['copy:fonts']);
+  gulp.watch(src + 'assets/videos/**/*.{mp4,mov}', ['copy:videos']);
+  gulp.watch(src + 'views/**/*.pug', ['pug']);
+});
+
+// Sync Browser on Change
+gulp.task('browserSync', function() {
+  browserSync({
+    ui: {
+      port: 2016
+    },
+    port: 2017,
+    // Clicks, Scrolls & Form inputs on any device will be mirrored to all others.
+    ghostMode: true,
+    server: {
+      baseDir: dist
+    },
+  })
+})
+
+// Default Task
+gulp.task('compile', ['pug', 'lint', 'sass', 'scripts']);
+gulp.task('copy', ['copy:images', 'copy:videos', 'copy:fonts']);
+gulp.task('default', ['pug', 'lint', 'sass', 'scripts', 'copy', 'watch']);
---
+++ src/assets/javascripts/main.js
@@ -0,0 +1,166 @@
+$(document).ready(function() { // Attend que la page ait chargé pour lancer le JS
+
+	$.fn.containerSlides = function(defaultTime) {
+		that = this;
+		var slides = that.find('.content-slide'),
+			current = 0,
+			nbOfSlides = slides.length;
+
+		function activate(index) {
+			slides.removeClass('active').eq(index).addClass('active');
+			var slideTime = slides.eq(index).data('time');
+			if (slideTime) {
+				setTimeout(next, slideTime);
+			} else {
+				setTimeout(next, defaultTime);
+			}
+		}
+
+		function next() {
+			if (current + 1 === nbOfSlides) {
+				current = 0;
+			} else {
+				current++;
+			}
+			activate(current);
+		}
+
+		activate(current);
+
+		return this;
+	}
+
+	$('body').containerSlides(30000);
+
+	// Nom des jours en français
+	var frenchDays = [
+		'lundi',
+		'mardi',
+		'mercredi',
+		'jeudi',
+		'vendredi',
+		'samedi',
+		'dimanche'
+	];
+
+	// Nom des mois en français
+	var frenchMonths = [
+		'janvien',
+		'février',
+		'mars',
+		'avril',
+		'mai',
+		'juin',
+		'juil',
+		'août',
+		'septembre',
+		'octobre',
+		'novembre',
+		'décembre'
+	];
+
+	// Day colors
+	var dayColors = [
+		'#377AF4',
+		'#F44E63',
+		'#4FE88D',
+		'#FFA543',
+		'#FDCC47',
+		'#377AF4',
+		'#377AF4'
+	];
+
+	// Fonction pour formatter l'heure en 18H ou 09H30, etc.
+	function formatHHMM(date) {
+		var hours = date.getHours();
+		var minutes = (date.getMinutes() == 0) ? '': ('0' + date.getMinutes()).slice(-2);
+		var strTime = hours + 'h' + minutes;
+		return strTime;
+	}
+
+	// Fonction pour mettre à jour la date et l'heure
+	function updateTime() {
+
+		// Récupère la date d'aujourd'hui et la met à jour
+		var today = new Date();
+		var $todayDiv = $('.navbar-date');
+		// Jour de la semaine
+		var day = frenchDays[today.getDay()-1];
+		$todayDiv.find('.day').html(day);
+		// Jour du mois
+		var date = ('0' + today.getDate()).slice(-2);
+		$todayDiv.find('.date').html(date);
+		// Mois
+		var month = frenchMonths[today.getMonth()];
+		$todayDiv.find('.month').html(month);
+		// Heure
+		var time = today.getHours() + 'H' + today.getMinutes();
+		$todayDiv.find('.time').html(formatHHMM(today));
+
+		// Set Color to day color
+		var dayColor = dayColors[today.getDay() - 1];
+		$('.dayColor').css('color', dayColor);
+		$('.dayBgColor').css('background-color', dayColor);
+	}
+
+	// Met à jour l'heure toutes les secondes
+	updateTime();
+	var updateTimeInterval = setInterval(updateTime, 1);
+
+	// Fonction pour charger les événements du jour
+	function updateScreen() {
+
+		var url = 'http://admin.theschoolab.com/api/v1/events/today'; // définit l'url de l'api
+
+		$.getJSON(url).done(function(data) { // fait une requète GET à l'API
+
+			$('#events').empty();
+
+			$.each(data, function(index, event) { // fait une boucle sur la liste des
+
+				// Clone le HTML exemple
+				var element = $('#hidden-event').clone();
+
+				// Retire l'id
+				element.attr('id', '');
+
+				// Ajoute chaque info de l'événement dans le HTML
+				for (var key in event) {
+					if (key == 'start_time' || key == 'end_time') {
+						var time = new Date(event[key]);
+						event[key] = formatHHMM(time);
+					}
+					element.find('.'+key).html(event[key]);
+				}
+
+				if (event['host'] == '') {
+					element.find('.eventhost').remove();
+				}
+
+				element.find('.image').remove();
+
+				// insère dans le HTML de la page
+				$('#events').append(element);
+			});
+
+		});
+
+	}
+	// Met à jour les events toutes les 10 minutes
+	updateScreen();
+	var updateScreenInterval = setInterval(updateScreen, 60000);
+
+	// Scroll auto
+  var scrolltopbottom =  setInterval(function(){
+  	var scrollDistance = $(document).height() - $(window).height();
+         // 4000 - it will take 4 secound in total from the top of the page to the bottom
+	  $("html, body").animate({ scrollTop: scrollDistance }, 10000);
+	  setTimeout(function() {
+	     $('html, body').animate({scrollTop:0}, 10000);
+	  },10000);
+
+  }, 20000);
+
+});
+
+ //function fullscreen
